abfedb0cc504ef1d0ee8eaf022e1674c4e2686d9
Florian D <florian.dindaud@orange.fr>
2024-03-17 16:47:20 +0100

    Initial commit

---
+++ js/liste_personnages.js
@@ -0,0 +1,91 @@
+let ligth =document.querySelector('.light')
+function switchMode(){
+
+    let footercolor = document.querySelector('#footerP')
+    let darkMode = document.querySelector('#darkMode')
+    let lightMode = document.querySelector('#lightMode')
+    let bodyColor = document.querySelector('#body')
+
+    if(darkMode.classList.contains('display')){
+        
+        lightMode.classList.add('display')
+        darkMode.classList.remove('display')
+        bodyColor.classList.add('dark')
+        bodyColor.classList.remove('light')
+        footercolor.classList.add('darkFooter')
+        footercolor.classList.remove('lightFooter')
+        
+       
+
+    } else{ 
+        lightMode.classList.remove('display')
+        darkMode.classList.add('display')
+        bodyColor.classList.remove('dark')
+        bodyColor.classList.add('light')
+        footercolor.classList.add('lightFooter')
+        footercolor.classList.remove('darkFooter')
+    }
+
+
+}
+/*-- tabs--*/
+
+var sidenav = document.getElementById("tabs");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
+
+/*-- carte --*/
+
+function fetchCharacters(){
+  return fetch('https://hp-api.lainocs.fr/characters')
+  .then ((response) => response.json())
+
+}
+
+async function displayCharacters(){
+  const data = await fetchCharacters()
+  data.forEach(character => {
+      document.querySelector('#characters').innerHTML += `
+      <div class='character' id="perso">
+         <a href="personnage.html?slug=${character.slug}">
+           <div class =" text">
+             <h3>${character.name}</h3>
+             <img class="image" src="${character.image}" alt="${character.name}">
+             <p class='text' id='house'>${character.house}</p>
+           </div>
+         </a>
+      
+      `
+  });
+  
+let element = document.getElementById ('house')
+ let colorBack = document.querySelector('.character')
+
+  if(characters.house == "Gryffindor"){
+    colorBack.classList.add('gryffindor')
+  } 
+  else if (characters.house =="Slytherin") {
+    colorBack.classList.add('slytherin')
+  }
+  else if (characters.house =="Ravenclaw") {
+    colorBack.classList.add('ravenclaw')
+  }
+  else if (characters.house =="Hufflepuff") {
+    colorBack.classList.add('hufflepuff')
+  }
+  
+}
+displayCharacters()
---
+++ js/personnage.js
@@ -0,0 +1,93 @@
+let ligth =document.querySelector('.light')
+function switchMode(){
+
+    let footercolor = document.querySelector('#footerP')
+    let darkMode = document.querySelector('#darkMode')
+    let lightMode = document.querySelector('#lightMode')
+    let bodyColor = document.querySelector('#body')
+
+    if(darkMode.classList.contains('display')){
+        
+        lightMode.classList.add('display')
+        darkMode.classList.remove('display')
+        bodyColor.classList.add('dark')
+        bodyColor.classList.remove('light')
+        footercolor.classList.add('darkFooter')
+        footercolor.classList.remove('lightFooter')
+        
+       
+
+    } else{ 
+        lightMode.classList.remove('display')
+        darkMode.classList.add('display')
+        bodyColor.classList.remove('dark')
+        bodyColor.classList.add('light')
+        footercolor.classList.add('lightFooter')
+        footercolor.classList.remove('darkFooter')
+    }
+
+
+}
+/*-- tabs--*/
+
+var sidenav = document.getElementById("tabs");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
+
+/*--carte --*/
+
+function fetchCharacter(){
+  let url = window.location.search
+  let slug = new URLSearchParams(url).get('slug')
+  return fetch('https://hp-api.lainocs.fr/characters/'+ slug)
+  .then((response) => response.json())
+
+}
+async function displayCharacter(){
+  const data = await fetchCharacter()
+  document.querySelector('#character').innerHTML = `
+  <div class='character'>
+  <h2 class =" texte "> ${data.name} </h2>
+  <img src="${data.image}" alt="${data.name}">
+  <p class='text' id='house'>${data.house}</p>
+  <p class='text'>${data.actor}</p>
+  <a class='text' href="liste_personnage.html">Back</a>
+  </div>
+  `
+ let element = document.getElementById ('house')
+ let colorBack = document.querySelector('.character')
+  if(data.house == "Gryffindor"){
+    colorBack.classList.add('gryffindor')
+  } 
+  else if (data.house =="Slytherin") {
+    colorBack.classList.add('slytherin')
+  }
+  else if (data.house =="Ravenclaw") {
+    colorBack.classList.add('ravenclaw')
+  }
+  else if (data.house =="Hufflepuff") {
+    colorBack.classList.add('hufflepuff')
+  }
+  else {
+    colorBack.classList.add('noHouse')
+  }
+
+  
+}
+displayCharacter()
+
+
+
