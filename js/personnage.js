a17a6b493915b69f2819ccfa0540773ac9181e8c
Florian D <florian.dindaud@orange.fr>
2024-05-04 21:14:15 +0200

    ajoute de l'iot et du data

--- js/personnage.js
+++ js/personnage.js
@@ -1,32 +1,25 @@
-let ligth =document.querySelector('.light')
-function switchMode(){
+let ligth = document.querySelector(".light");
+function switchMode() {
+  let footercolor = document.querySelector("#footerP");
+  let darkMode = document.querySelector("#darkMode");
+  let lightMode = document.querySelector("#lightMode");
+  let bodyColor = document.querySelector("#body");
 
-    let footercolor = document.querySelector('#footerP')
-    let darkMode = document.querySelector('#darkMode')
-    let lightMode = document.querySelector('#lightMode')
-    let bodyColor = document.querySelector('#body')
-
-    if(darkMode.classList.contains('display')){
-        
-        lightMode.classList.add('display')
-        darkMode.classList.remove('display')
-        bodyColor.classList.add('dark')
-        bodyColor.classList.remove('light')
-        footercolor.classList.add('darkFooter')
-        footercolor.classList.remove('lightFooter')
-        
-       
-
-    } else{ 
-        lightMode.classList.remove('display')
-        darkMode.classList.add('display')
-        bodyColor.classList.remove('dark')
-        bodyColor.classList.add('light')
-        footercolor.classList.add('lightFooter')
-        footercolor.classList.remove('darkFooter')
-    }
-
-
+  if (darkMode.classList.contains("display")) {
+    lightMode.classList.add("display");
+    darkMode.classList.remove("display");
+    bodyColor.classList.add("dark");
+    bodyColor.classList.remove("light");
+    footercolor.classList.add("darkFooter");
+    footercolor.classList.remove("lightFooter");
+  } else {
+    lightMode.classList.remove("display");
+    darkMode.classList.add("display");
+    bodyColor.classList.remove("dark");
+    bodyColor.classList.add("light");
+    footercolor.classList.add("lightFooter");
+    footercolor.classList.remove("darkFooter");
+  }
 }
 /*-- tabs--*/
 
@@ -49,16 +42,37 @@ function closeNav() {
 
 /*--carte --*/
 
-function fetchCharacter(){
-  let url = window.location.search
-  let slug = new URLSearchParams(url).get('slug')
-  return fetch('https://hp-api.lainocs.fr/characters/'+ slug)
-  .then((response) => response.json())
-
+function fetchCharacter() {
+  let url = window.location.search;
+  let slug = new URLSearchParams(url).get("slug");
+  return fetch("https://hp-api.lainocs.fr/characters/" + slug).then(
+    (response) => response.json()
+  );
 }
-async function displayCharacter(){
-  const data = await fetchCharacter()
-  document.querySelector('#character').innerHTML = `
+let updateData = (_house) => {
+  const data = {
+    house: _house,
+  };
+  fetch("http://192.168.69.24:3000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+async function displayCharacter() {
+  const data = await fetchCharacter();
+  document.querySelector("#character").innerHTML = `
   <div class='character'>
   <h2 class =" texte "> ${data.name} </h2>
   <img src="${data.image}" alt="${data.name}">
@@ -66,28 +80,21 @@ async function displayCharacter(){
   <p class='text'>${data.actor}</p>
   <a class='text' href="liste_personnage.html">Back</a>
   </div>
-  `
- let element = document.getElementById ('house')
- let colorBack = document.querySelector('.character')
-  if(data.house == "Gryffindor"){
-    colorBack.classList.add('gryffindor')
-  } 
-  else if (data.house =="Slytherin") {
-    colorBack.classList.add('slytherin')
+  `;
+  let element = document.getElementById("house");
+  let colorBack = document.querySelector(".character");
+  if (data.house == "Gryffindor") {
+    colorBack.classList.add("gryffindor");
+  } else if (data.house == "Slytherin") {
+    colorBack.classList.add("slytherin");
+  } else if (data.house == "Ravenclaw") {
+    colorBack.classList.add("ravenclaw");
+  } else if (data.house == "Hufflepuff") {
+    colorBack.classList.add("hufflepuff");
+  } else {
+    colorBack.classList.add("noHouse");
   }
-  else if (data.house =="Ravenclaw") {
-    colorBack.classList.add('ravenclaw')
-  }
-  else if (data.house =="Hufflepuff") {
-    colorBack.classList.add('hufflepuff')
-  }
-  else {
-    colorBack.classList.add('noHouse')
-  }
-
-  
+  updateData(data.house);
 }
-displayCharacter()
 
-
-
+displayCharacter();
--- node js/index.js
+++ node js/index.js
@@ -3,13 +3,26 @@ const routes = require("./routes/start");
 const cors = require("cors");
 const app = express();
 const port = 3000;
+const ip = require("ip");
+const ipAddr = ip.address();
+const bodyParser = require("body-parser");
 
-app.use(express.json());
+let lastHouseVisited = "Gryffondor";
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use("/", routes);
 app.use(cors());
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/", (req, res) => {
+  console.log("get");
+  res.json({ house: lastHouseVisited });
+});
+app.post("/", (req, res) => {
+  lastHouseVisited = req.body.house; ///-----------------------
+  res.json({ house: lastHouseVisited }); // ----------------------
+});
+app.listen(3000, () => {
+  console.log("Server run: http://" + ipAddr + ":3000"); //--------------------
 });
 
 // /users en GET = tout les utilisateurs
